8cec17d9613e5d293fb4545261b43d7ddda8eca9
DevDeived <david.fisica@yahoo.com.br>
2025-10-26 00:23:59 -0300

    to-do-Backend

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import listRouter from '../routes/lists.js'; 
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import listRouter from "./routes/lists.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: 'https://lista-compras.vercel.app', 
+    origin: "http://localhost:5173",
   })
 );
 
 app.use(express.json());
 
-app.use('/api/lists', listRouter);
+app.use("/api/lists", listRouter);
 
-app.get('/', (req, res) => {
-  res.json({ status: 'ok', env: process.env.NODE_ENV || 'dev' });
+app.get("/", (req, res) => {
+  res.json({ status: "ok", env: process.env.NODE_ENV || "dev" });
 });
 
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na Http://localhost:${PORT}`);
+});
